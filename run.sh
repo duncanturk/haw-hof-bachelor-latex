c246cb66893f969e752689d6e7d47b79a9acdef4
Christopher Rossbach <chr.rossbach8@googlemail.com>
2019-10-09 18:34:10 +0200

    use new image

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-docker run --rm -v $PWD:/tmp duncanturk/ubuntu-latex:16.04 make "$@"
-docker run --rm -v $PWD:/tmp duncanturk/ubuntu-latex:16.04 chown $UID:$(id -g $UID) . -R
+docker run --rm -v $PWD:/tmp duncanturk/ubuntu-latex:19.04 make "$@"
+docker run --rm -v $PWD:/tmp duncanturk/ubuntu-latex:19.04 chown $UID:$(id -g $UID) . -R
